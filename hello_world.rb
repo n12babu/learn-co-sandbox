1318a6f6675b4efe1c6df820e057e048d1f9f546
Nazrul Babu <n12babu@siena.edu>
2018-11-26 18:55:50 +0000

    automatically backed up by learn

---
+++ hello_world.rb
@@ -0,0 +1,94 @@
+
+
+
+
+#def hello (name)
+#interpolation mean we can put the varuibqale in our string but still tell ruby that it's a varible and not a l 
+#puts "Hello #{name}"
+
+#end 
+
+
+#when run this method we went it to print "Hello name "
+
+#name = "Nazrul"
+
+#String vs code 
+
+hello (name)
+
+#--------------------------------------------------------------------------
+
+#def add (arg1, arg2);
+  #return arg1+arg2
+  
+#end 
+
+
+# add 1+1 and 2 as the result
+
+#add(1,1)
+
+
+
+#def add (arg1, arg2);
+  #puts arg1+arg2
+  
+#end 
+
+
+#def subtract (arg1, arg2);
+  #puts arg1-arg2
+  
+#end 
+
+#def multiply (arg1, arg2);
+  puts arg1*arg2
+  
+#end 
+
+#def divide (arg1, arg2);
+  puts arg1/arg2
+  
+#end 
+
+#def exp (arg1);
+  puts arg1**2
+  
+#end 
+
+#add(1,1)
+#subtract(3,1)
+#multiply(4,1)
+#divide(4,1)
+#exp(7)
+
+
+#-------------------------------
+#name="Naz"
+
+#if name =="Naz"
+  #puts "hello#{name}"
+  
+  
+#end 
+#-------------------------------
+#puts "hello! Whats your name?"
+#input= gets.chomp
+
+
+
+#def greet (name)
+#if name =="Naz"
+ # puts "hello#{name}"
+#else puts "hello There"
+  
+#end 
+#end
+
+
+number = 10;
+number.times do
+  puts "Nazrul"
+end
+
